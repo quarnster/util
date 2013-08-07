e7f6eb2593f7dd32e3e1d89d3688e331dcbbf402
Fredrik Ehnbom <fredrik@ehnbom.nu>
2013-08-07 16:09:12 +0200

    Binary tree: can now delete nodes from it

--- container/binarytree.go
+++ container/binarytree.go
@@ -1,45 +1,104 @@
 package container
 
+const (
+	Less ComparisonResult = iota
+	Equal
+	Greater
+)
+
 type (
-	Node struct {
+	ComparisonResult int
+	Node             struct {
 		Data     interface{}
-		Children []Node
+		Children [2]*Node
 	}
-	Less func(a, b interface{}) bool
-	Tree struct {
-		Less Less
-		Root Node
+	Compare func(a, b interface{}) ComparisonResult
+	Tree    struct {
+		Compare Compare
+		Root    Node
 	}
 )
 
-func (n *Node) Find(data interface{}, less Less) *Node {
+func (n *Node) find(data interface{}, cmp Compare, child int, parent *Node) (rchild int, retparent, node *Node) {
 	if n.Data == nil {
-		return n
-	} else {
-		if n.Children == nil {
-			n.Children = make([]Node, 2)
-		}
-		if less(data, n.Data) {
-			return n.Children[0].Find(data, less)
+		return child, parent, n
+	}
+	switch c := cmp(data, n.Data); c {
+	case Equal:
+		return child, parent, n
+	case Less:
+		if n.Children[0] == nil {
+			return 0, n, n.Children[0]
 		} else {
-			return n.Children[1].Find(data, less)
+			return n.Children[0].find(data, cmp, 0, n)
 		}
+	case Greater:
+		if n.Children[1] == nil {
+			return 1, n, n.Children[1]
+		} else {
+			return n.Children[1].find(data, cmp, 1, n)
+		}
+	default:
+		panic(c)
 	}
 }
 
+func (n *Node) Find(data interface{}, cmp Compare) (child int, parent, node *Node) {
+	return n.find(data, cmp, -1, nil)
+}
+
 func (n *Node) Walk(ch chan interface{}) {
-	if n.Children != nil {
+	if n.Children[0] != nil {
 		n.Children[0].Walk(ch)
 	}
 	if n.Data != nil {
 		ch <- n.Data
 	}
-	if n.Children != nil {
+	if n.Children[1] != nil {
 		n.Children[1].Walk(ch)
 	}
 }
 
+func (n *Node) delete(child int, parent *Node) {
+	a, b := n.Children[0], n.Children[1]
+	switch {
+	case a == nil && b == nil:
+		if parent != nil {
+			parent.Children[child] = nil
+		} else {
+			n.Data = nil
+		}
+	case a == nil && b != nil:
+		*n = *b
+	case a != nil && b != nil:
+		*n = *a
+	default:
+		if ac := a.Children[1]; ac != nil {
+			n.Data = ac.Data
+			ac.delete(1, a)
+		} else if bc := b.Children[0]; bc != nil {
+			n.Data = bc.Data
+			bc.delete(0, b)
+		}
+	}
+}
+
 func (t *Tree) Add(data interface{}) {
-	n := t.Root.Find(data, t.Less)
-	n.Data = data
+	child, p, n := t.Root.Find(data, t.Compare)
+	if n != nil {
+		n.Data = data
+	} else if p.Data != nil {
+		p.Children[child] = &Node{Data: data}
+	} else {
+		panic("Both parent and child was null")
+	}
+}
+
+func (t *Tree) Delete(data interface{}) {
+	child, p, n := t.Root.Find(data, t.Compare)
+	if n == nil {
+		panic("Unable to find that node")
+	} else {
+		n.delete(child, p)
+	}
 }
--- container/binarytree_test.go
+++ container/binarytree_test.go
@@ -8,12 +8,102 @@ import (
 func TestBinaryTree(t *testing.T) {
 	const count = 100
 	for i := 0; i < 10; i++ {
-		tree := Tree{Less: func(a, b interface{}) bool {
-			return a.(int) < b.(int)
+		tree := Tree{Compare: func(a, b interface{}) ComparisonResult {
+			aa := a.(int)
+			bb := b.(int)
+			switch {
+			case aa < bb:
+				return Less
+			case aa > bb:
+				return Greater
+			default:
+				return Equal
+			}
 		}}
 		list := rand.Perm(count)
 		for _, j := range list {
 			tree.Add(j)
+		}
+
+		ch := make(chan interface{})
+		go func() {
+			tree.Root.Walk(ch)
+			close(ch)
+		}()
+		for j := 0; j < count; j++ {
+			k := (<-ch).(int)
+			if k != j {
+				t.Errorf("%d != %d", k, j)
+			}
+		}
+	}
+}
+
+func TestBinaryTreeDelete(t *testing.T) {
+	const count = 100
+	const sub = 20
+	for i := 0; i < 10; i++ {
+		tree := Tree{Compare: func(a, b interface{}) ComparisonResult {
+			aa := a.(int)
+			bb := b.(int)
+			switch {
+			case aa < bb:
+				return Less
+			case aa > bb:
+				return Greater
+			default:
+				return Equal
+			}
+		}}
+		list := rand.Perm(count)
+		for _, j := range list {
+			tree.Add(j)
+		}
+		for j := 0; j < sub; j++ {
+			tree.Delete(j)
+		}
+
+		ch := make(chan interface{})
+		go func() {
+			tree.Root.Walk(ch)
+			close(ch)
+		}()
+		for j := 0; j < count-sub; j++ {
+			k := (<-ch).(int)
+			if k != (j + sub) {
+				t.Errorf("%d != %d", k, j+sub)
+			}
+		}
+	}
+}
+
+func TestBinaryTreeAddDelete(t *testing.T) {
+	const count = 100
+	const sub = 20
+	for i := 0; i < 10; i++ {
+		tree := Tree{Compare: func(a, b interface{}) ComparisonResult {
+			aa := a.(int)
+			bb := b.(int)
+			switch {
+			case aa < bb:
+				return Less
+			case aa > bb:
+				return Greater
+			default:
+				return Equal
+			}
+		}}
+		list := rand.Perm(count)
+		for _, j := range list {
+			tree.Add(j)
+		}
+		for k := 0; k < 10; k++ {
+			for j := 0; j < sub; j++ {
+				tree.Delete(j)
+			}
+			for j := 0; j < sub; j++ {
+				tree.Add(j)
+			}
 		}
 
 		ch := make(chan interface{})
